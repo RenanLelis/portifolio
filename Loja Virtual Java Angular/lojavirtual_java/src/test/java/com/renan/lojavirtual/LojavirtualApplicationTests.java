5a7f3429c6207eb1283637b30cdbadb401199821
Renan Lelis Rufino dos Santos <renan@MacBook-Pro-de-Renan.local>
2023-01-24 09:42:56 -0300

    .

---
+++ Loja Virtual Java Angular/lojavirtual_java/src/test/java/com/renan/lojavirtual/LojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
