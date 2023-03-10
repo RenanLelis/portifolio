ccac37b632eced8880c9cdec50920c12055c8847
Renan Lelis <renan.lelis@gmail.com>
2023-03-10 16:45:36 -0300

    .

---
+++ to-do app/to-do-angular/src/app/message/message.ts
@@ -0,0 +1,40 @@
+export function getMessage(codigo: string): string {
+    if (messages.has(codigo)) {
+        return messages.get(codigo)!;
+    } else {
+        return messages.get("MSE01")!;
+    }
+}
+
+export function getErrorMessage(): string {
+    return messages.get("MSE01")!;
+}
+
+export function getErrorMessageInputValues(): string {
+    return messages.get("MSE02")!;
+}
+
+export function getErrorMessageInvalidToken(): string {
+    return messages.get("MSE03")!;
+}
+
+export function getErrorMessageLogin(): string {
+    return messages.get("MSE04")!;
+}
+
+export function getErrorMessageEmailExists(): string {
+    return messages.get("MSE05")!;
+}
+
+export function getErrorMessageEmailNotFound(): string {
+    return messages.get("MSE06")!;
+}
+
+export const messages: Map<string, string> = new Map<string, string>([
+    ["MSE01", "An error ocurred on the operation."],
+    ["MSE02", "Error on input values."],
+    ["MSE03", "Invalid Authentication Token."],
+    ["MSE04", "Invalid User or Password."],
+    ["MSE05", "E-mail already exists."],
+    ["MSE06", "E-mail not found."],
+]);
--- to-do app/to-do-angular/src/app/view/auth/login/login.component.ts
+++ to-do app/to-do-angular/src/app/view/auth/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  formLogin: FormGroup = new FormGroup({
+    "email": new FormControl('', [Validators.email, Validators.required]),
+    "password": new FormControl('', [Validators.required, Validators.minLength(5), this.passwordValidation]),
+  });
+
+  loading: boolean = false;
+  errorMessage: string = "";
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    if (this.authService.isUserLoggedIn()) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  login() {}
+
+  closeErrorMessage() {
+    this.errorMessage = "";
+  }
+
+  passwordValidation(control: FormControl) {
+    if (control.value === null || control.value === '' || !/\d/.test(control.value)) {
+      return { "invalidPassword": true };
+    }
+    return null;
+  }
+
 
 }
