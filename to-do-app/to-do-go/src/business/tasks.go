3894d43bdfea25d1ec33be0556a21ff86296f7d8
Renan Lelis <renan.lelis.profissional@gmail.com>
2023-04-21 23:04:48 -0300

    to-do-go refactor

--- to-do-app/to-do-go/src/business/tasks.go
+++ to-do-app/to-do-go/src/business/tasks.go
@@ -9,45 +9,74 @@ import (
 	"renanlelis.github.io/portfolio/to-do-go/src/business/messages"
 	"renanlelis.github.io/portfolio/to-do-go/src/controller/response"
 	"renanlelis.github.io/portfolio/to-do-go/src/model"
+	"renanlelis.github.io/portfolio/to-do-go/src/persistence/dao"
 )
 
-// GetTasksAndLists implements the controller to get tasks and lists
+// GetTasksAndLists get tasks and lists for the user
 func GetTasksAndLists(userID uint64) ([]response.TaskListDTO, Err) {
 	if userID <= 0 {
-		return nil, Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
+		return nil, Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
 	}
-	//TODO
-	return nil, Err{}
+	lists, tasks, err := dao.GetTasksAndLists(userID)
+	if err != nil {
+		return nil, Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
+	taskListDTO := response.ConvertTasksAndListsToDTO(lists, tasks)
+	return taskListDTO, Err{}
 }
 
-// GetLists implements the controller to get lists
+// GetLists get lists for the user
 func GetLists(userID uint64) ([]response.TaskListDTO, Err) {
 	if userID <= 0 {
-		return nil, Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
+		return nil, Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
 	}
-	//TODO
-	return nil, Err{}
+	lists, err := dao.GetLists(userID)
+	if err != nil {
+		return nil, Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
+	taskListDTO := response.ConvertTasksAndListsToDTO(lists, make([]model.Task, 0))
+	return taskListDTO, Err{}
 }
 
 // GetTasksByList return tasks by a selected list
 func GetTasksByList(listID, userID uint64) ([]response.TaskDTO, Err) {
-	if listID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return nil, Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if listID <= 0 {
 		return nil, Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
-	return nil, Err{}
+	tasks, err := dao.GetTasksByList(listID, userID)
+	if err != nil {
+		return nil, Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
+	tasksDTO := response.ConvertTasksToDTO(tasks)
+	return tasksDTO, Err{}
 }
 
 // CreateTaskList create a new list for the user on the database and return the list created
 func CreateTaskList(listName, listDescription string, userID uint64) (response.TaskListDTO, Err) {
+	if userID <= 0 {
+		return response.TaskListDTO{}, Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
 	listNameFormatted := strings.TrimSpace(listName)
 	listDescriptionFormatted := strings.TrimSpace(listDescription)
 	err := validateTaskListData(listNameFormatted, listDescriptionFormatted, userID)
 	if err != nil {
 		return response.TaskListDTO{}, Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
-	return response.TaskListDTO{}, Err{}
+	newID, err := dao.CreateNewList(listNameFormatted, listDescriptionFormatted, userID)
+	if err != nil {
+		return response.TaskListDTO{}, Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
+	taskList := model.TaskList{
+		ID:              newID,
+		ListName:        listNameFormatted,
+		ListDescription: listDescriptionFormatted,
+		UserID:          userID,
+	}
+	taskListDTO := response.ConvertTaskListToDTO(taskList)
+	return taskListDTO, Err{}
 }
 
 // validateTaskListData validate the input data for taskList Creation
@@ -60,60 +89,107 @@ func validateTaskListData(listName, listDescription string, userID uint64) error
 
 // UpdateTaskList update a list for the user on the database
 func UpdateTaskList(listName, listDescription string, listID, userID uint64) Err {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
 	listNameFormatted := strings.TrimSpace(listName)
 	listDescriptionFormatted := strings.TrimSpace(listDescription)
 	if err := validateTaskListData(listNameFormatted, listDescriptionFormatted, userID); err != nil || listID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.UpdateTaskList(listNameFormatted, listDescriptionFormatted, listID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // DeleteTaskList delete a list for the user and the tasks related
 func DeleteTaskList(listID, userID uint64) Err {
-	if listID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if listID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.DeleteTaskList(listID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // CompleteTasksFromList mark as complete all tasks from a list
 func CompleteTasksFromList(listID, userID uint64) Err {
-	if listID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if listID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.CompleteTasksFromList(listID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // UncompleteTasksFromList mark as incomplete all tasks from a list
 func UncompleteTasksFromList(listID, userID uint64) Err {
-	if listID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if listID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.UncompleteTasksFromList(listID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // MoveTasksFromList move the tasks from a list to another
 func MoveTasksFromList(listIDOrigin, listIDDestiny, userID uint64) Err {
-	if listIDOrigin <= 0 || listIDDestiny <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if listIDOrigin <= 0 || listIDDestiny <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.MoveTasksFromList(listIDOrigin, listIDDestiny, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // CreateTask create a new task for the user on the database and return the task created
 func CreateTask(taskName, taskDescription, deadline string, listID, userID uint64) (response.TaskDTO, Err) {
+	if userID <= 0 {
+		return response.TaskDTO{}, Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
 	taskNameFormatted := strings.TrimSpace(taskName)
 	taskDescriptionFormatted := strings.TrimSpace(taskDescription)
-	if err := validateTaskData(taskNameFormatted, taskDescriptionFormatted, deadline, userID); err != nil || listID <= 0 {
+	deadlineFormatted := strings.TrimSpace(deadline)
+	if err := validateTaskData(taskNameFormatted, taskDescriptionFormatted, deadlineFormatted, userID); err != nil || listID <= 0 {
 		return response.TaskDTO{}, Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
-	return response.TaskDTO{}, Err{}
+	newID, err := dao.CreateTask(taskNameFormatted, taskDescriptionFormatted, deadlineFormatted, listID, userID)
+	if err != nil {
+		return response.TaskDTO{}, Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
+	task := model.Task{
+		ID:              newID,
+		TaskName:        taskNameFormatted,
+		TaskDescription: taskDescriptionFormatted,
+		Deadline:        deadlineFormatted,
+		TaskStatus:      model.STATUS_TASK_INCOMPLETE,
+		ListID:          listID,
+		UserID:          userID,
+	}
+	taskDTO := response.ConvertTaskToDTO(task)
+	return taskDTO, Err{}
 }
 
 // validateTaskData validate the input data for task Creation
@@ -139,48 +215,79 @@ func IsDeadlineInvalid(deadline string) bool {
 
 // UpdateTask update a task for the user on the database
 func UpdateTask(taskName, taskDescription, deadline string, taskID, userID uint64) Err {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
 	taskNameFormatted := strings.TrimSpace(taskName)
 	taskDescriptionFormatted := strings.TrimSpace(taskDescription)
-	if err := validateTaskData(taskNameFormatted, taskDescriptionFormatted, deadline, userID); err != nil {
+	deadlineFormatted := strings.TrimSpace(deadline)
+	if err := validateTaskData(taskNameFormatted, taskDescriptionFormatted, deadlineFormatted, userID); err != nil {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.UpdateTask(taskNameFormatted, taskDescriptionFormatted, deadlineFormatted, taskID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // MoveTaskToList update a task for the user on the database, changing the listID
 func MoveTaskToList(taskID, listIDDestiny, userID uint64) Err {
-	if taskID <= 0 || listIDDestiny <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if taskID <= 0 || listIDDestiny <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.MoveTaskToList(taskID, listIDDestiny, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // DeleteTask delete a task for the user on the database
 func DeleteTask(taskID, userID uint64) Err {
-	if taskID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if taskID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.DeleteTask(taskID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // CompleteTask mark a task as complete for the user on the database
 func CompleteTask(taskID, userID uint64) Err {
-	if taskID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if taskID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.CompleteTask(taskID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
 // UncompleteTask mark a task as incomplete for the user on the database
 func UncompleteTask(taskID, userID uint64) Err {
-	if taskID <= 0 || userID <= 0 {
+	if userID <= 0 {
+		return Err{Status: http.StatusUnauthorized, ErrorMessage: messages.GetErrorMessageToken()}
+	}
+	if taskID <= 0 {
 		return Err{Status: http.StatusBadRequest, ErrorMessage: messages.GetErrorMessageInputValues()}
 	}
-	//TODO
+	err := dao.UncompleteTask(taskID, userID)
+	if err != nil {
+		return Err{http.StatusInternalServerError, messages.GetErrorMessage()}
+	}
 	return Err{}
 }
 
--- to-do-app/to-do-go/src/persistence/dao/task_list_dao.go
+++ to-do-app/to-do-go/src/persistence/dao/task_list_dao.go
@@ -19,84 +19,172 @@ func CreateNewList(listName, listDescription string, userID uint64) (uint64, err
 
 // GetTasksAndLists implements the controller to get tasks and lists
 func GetTasksAndLists(userID uint64) ([]model.TaskList, []model.Task, error) {
-	//TODO
-	return nil, nil, nil
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer db.Close()
+	rep := repository.CreateTaskListRepository(db)
+	lists, err := rep.GetTaskListsByUser(userID)
+	if err != nil {
+		return nil, nil, err
+	}
+	tasks, err := rep.GetTasksByUser(userID)
+	if err != nil {
+		return nil, nil, err
+	}
+	return lists, tasks, nil
 }
 
 // GetLists implements the controller to get lists
 func GetLists(userID uint64) ([]model.TaskList, error) {
-	//TODO
-	return nil, nil
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	rep := repository.CreateTaskListRepository(db)
+	return rep.GetTaskListsByUser(userID)
 }
 
 // GetTasksByList return tasks by a selected list
-func GetTasksByList(listID, userID uint64) ([]model.TaskList, error) {
-	//TODO
-	return nil, nil
+func GetTasksByList(listID, userID uint64) ([]model.Task, error) {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	rep := repository.CreateTaskListRepository(db)
+	return rep.GetTasksByList(listID, userID)
 }
 
 // UpdateTaskList update a list for the user on the database
 func UpdateTaskList(listName, listDescription string, listID, userID uint64) error {
-	//TODO
-	return nil
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	rep := repository.CreateTaskListRepository(db)
+	return rep.UpdateList(listID, userID, listName, listDescription)
 }
 
 // DeleteTaskList delete a list for the user and the tasks related
 func DeleteTaskList(listID, userID uint64) error {
-	//TODO
-	return nil
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	rep := repository.CreateTaskListRepository(db)
+	return rep.DeleteList(listID, userID)
 }
 
 // CompleteTasksFromList mark as complete all tasks from a list
 func CompleteTasksFromList(listID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // UncompleteTasksFromList mark as incomplete all tasks from a list
 func UncompleteTasksFromList(listID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // MoveTasksFromList move the tasks from a list to another
 func MoveTasksFromList(listIDOrigin, listIDDestiny, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
+	// return rep.MoveTasksFromList(listIDOrigin, listIDDestiny, userID)
 	//TODO
 	return nil
 }
 
 // CreateTask create a new task for the user on the database and return the task created
 func CreateTask(taskName, taskDescription, deadline string, listID, userID uint64) (uint64, error) {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return 0, nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return 0, nil
 }
 
 // UpdateTask update a task for the user on the database
 func UpdateTask(taskName, taskDescription, deadline string, taskID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // MoveTaskToList update a task for the user on the database, changing the listID
 func MoveTaskToList(taskID, listIDDestiny, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // DeleteTask delete a task for the user on the database
 func DeleteTask(taskID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // CompleteTask mark a task as complete for the user on the database
 func CompleteTask(taskID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
 
 // UncompleteTask mark a task as incomplete for the user on the database
 func UncompleteTask(taskID, userID uint64) error {
+	db, err := database.ConnectToDataBase()
+	if err != nil {
+		return nil
+	}
+	defer db.Close()
+	// rep := repository.CreateTaskListRepository(db)
 	//TODO
 	return nil
 }
