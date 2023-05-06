a07fe5959a448e4334aebc7c126951d457b31652
Renan Lelis <renan.lelis.profissional@gmail.com>
2023-05-06 19:51:32 -0300

    fix-docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -27,19 +27,3 @@ for (var i = 0; i < navLinks.length; i++) {
         body.style.overflow = "auto";
     });
 }
-
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
